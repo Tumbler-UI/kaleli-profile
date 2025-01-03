504904c0c97649dfb19c8a103a61700c6f1631ce
Tumbler-UI <joekaleli9@gmail.com>
2025-01-03 13:22:42 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hi I'm Joe Kaleli Mburu</h2>
+      <h3>Full Stack Web Developer</h3>
+      <p>I am a software engineer based in Nairobi, Kenya</p>
+      <p>Contact me via whatsapp +254 751 306 863</p>
     </div>
   );
 }
