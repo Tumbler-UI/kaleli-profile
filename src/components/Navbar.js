e3f21c23a742d6c4502b73f5018e0a3233d11c43
Tumbler-UI <joekaleli9@gmail.com>
2025-03-05 02:21:42 +0300

    edited logo message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">KAVALI.DEV</div>
+      <div className="logo">Kavali.Dev</div>
       <ul className="nav-links">
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
