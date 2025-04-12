17a3bf17ce223fd60524d61ae1406b688e48f662
Tumbler-UI <joekaleli9@gmail.com>
2025-04-12 16:58:55 +0300

    Removed the contact section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Hero />
       <About />
       <Projects />
-      <Contact />
       <Footer />
     </div>
   );
