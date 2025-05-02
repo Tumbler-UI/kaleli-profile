69404eac5fb277be7e9d292b18756e131754a726
Tumbler-UI <joekaleli9@gmail.com>
2025-05-02 09:18:29 +0300

    Removed contact section in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,6 @@ function Navbar() {
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   );
